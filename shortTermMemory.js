93420fb0ff0dc91e4616cb0521e65917e6303bd3
kerringtonwells <kwells2@live.com>
2023-08-23 16:11:09 -0400

    Update shortTermMemory.js

--- shortTermMemory.js
+++ shortTermMemory.js
@@ -1,4 +1,9 @@
 let generatedItems = [];
+let activeTimeout = null;
+let activeInterval = null;
+let correctCounter = 0;
+let incorrectCounter = 0;
+
 
 function getRandomWord() {
     const     
@@ -24,6 +29,14 @@ function getRandomWord() {
     }
 
     function startGame() {
+        // Clear any active timers from previous games
+        if (activeTimeout) {
+            clearTimeout(activeTimeout);
+        }
+        if (activeInterval) {
+            clearInterval(activeInterval);
+        }
+    
         const type = document.getElementById("type").value;
         const quantity = parseInt(document.getElementById("quantity").value);
         const duration = parseInt(document.getElementById("duration").value) * 1000;
@@ -31,57 +44,80 @@ function getRandomWord() {
         generatedItems = [];
     
         for (let i = 0; i < quantity; i++) {
-        if (type === "numbers") {
-            generatedItems.push(Math.floor(Math.random() * 100).toString());
-        } else if (type === "words") {
-            generatedItems.push(getRandomWord());
-        } else {
-            const choice = Math.random() < 0.5 ? "number" : "word";
-            if (choice === "number") {
+            if (type === "numbers") {
                 generatedItems.push(Math.floor(Math.random() * 100).toString());
-            } else {
+            } else if (type === "words") {
                 generatedItems.push(getRandomWord());
+            } else {
+                const choice = Math.random() < 0.5 ? "number" : "word";
+                if (choice === "number") {
+                    generatedItems.push(Math.floor(Math.random() * 100).toString());
+                } else {
+                    generatedItems.push(getRandomWord());
+                }
             }
         }
-    }
-
-    document.getElementById("displayArea").innerText = generatedItems.join(", ");
-    setTimeout(() => {
-        document.getElementById("displayArea").innerText = "Now, try to remember!";
-        createInputBoxes(quantity);
-    }, duration);
-}
-
-function createInputBoxes(quantity) {
-    const inputArea = document.getElementById("inputArea");
-    inputArea.innerHTML = "";
-    for (let i = 0; i < quantity; i++) {
-        const inputBox = document.createElement("input");
-        inputBox.setAttribute("type", "text");
-        inputBox.setAttribute("id", "box" + i);
-        inputArea.appendChild(inputBox);
-    }
-    const submitButton = document.createElement("button");
-    submitButton.innerText = "Check Answers";
-    submitButton.onclick = checkAnswers;
-    inputArea.appendChild(submitButton);
-}
-
-function checkAnswers() {
-    let correct = true;
-    for (let i = 0; i < generatedItems.length; i++) {
-        const inputValue = document.getElementById("box" + i).value;
-        if (inputValue !== generatedItems[i]) {
-            correct = false;
-            break;
-        }
-    }
-    if (correct) {
-        alert("Correct!");
-    } else {
-        alert("Wrong! The correct answers were: " + generatedItems.join(", "));
+    
+        // Start the countdown before showing the items
+        let countdownTime = 3;  // 3 seconds countdown
+        document.getElementById("displayArea").innerText = "Starting in: " + countdownTime;
+    
+        activeInterval = setInterval(() => {
+            countdownTime--;
+            if(countdownTime > 0) {
+                document.getElementById("displayArea").innerText = "Starting in: " + countdownTime;
+            } else {
+                clearInterval(activeInterval);
+                document.getElementById("displayArea").innerText = generatedItems.join(", ");
+                activeTimeout = setTimeout(() => {
+                    document.getElementById("displayArea").innerText = "Now, try to remember!";
+                    createInputBoxes(quantity);
+                }, duration);
+            }
+        }, 1000);
     }
     
-    // Wait for 2 seconds, then start the next round
-    setTimeout(startGame, 2000);
-}
+
+    function createInputBoxes(quantity) {
+        const inputArea = document.getElementById("inputArea");
+        inputArea.innerHTML = "";
+        for (let i = 0; i < quantity; i++) {
+            const inputBox = document.createElement("input");
+            inputBox.setAttribute("type", "text");
+            inputBox.setAttribute("id", "box" + i);
+            inputArea.appendChild(inputBox);
+        }
+        const submitButton = document.createElement("button");
+        submitButton.innerText = "Check Answers";
+        submitButton.onclick = checkAnswers;
+        inputArea.appendChild(submitButton);
+    
+        // Set focus on the first input box
+        document.getElementById("box0").focus();
+    }
+    
+
+    function checkAnswers() {
+        let correct = true;
+        for (let i = 0; i < generatedItems.length; i++) {
+            const inputValue = document.getElementById("box" + i).value;
+            if (inputValue !== generatedItems[i]) {
+                correct = false;
+                break;
+            }
+        }
+        
+        if (correct) {
+            correctCounter++;
+            document.getElementById("correctCount").innerText = "Correct: " + correctCounter;
+            alert("Correct!");
+        } else {
+            incorrectCounter++;
+            document.getElementById("incorrectCount").innerText = "Incorrect: " + incorrectCounter;
+            alert("Wrong! The correct answers were: " + generatedItems.join(", "));
+        }
+    
+        // Wait for 2 seconds, then start the next round
+        setTimeout(startGame, 2000);
+    }
+    
